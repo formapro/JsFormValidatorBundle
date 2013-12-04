40b5736a8f31521f59397401d5607878c67c283d
Yury Maltsev <dev.yury.maltsev@gmail.com>
2013-12-04 15:44:40 +0200

    travis config fixes

--- Tests/app/index.php
+++ Tests/app/index.php
@@ -1,8 +1,8 @@
 <?php
-
+var_dump('___________we are here!!!!!!!!!!');die;
 $loader = require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/AppKernel.php';
-var_dump('___________we are here!!!!!!!!!!');die;
+
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('dev', true);
