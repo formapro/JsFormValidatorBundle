b38d8556244ff3b626bc4e3b307c74afe273ac83
ymalcev <dev.yury.maltsev@gmail.com>
2014-08-16 14:16:15 +0300

    changes for special versions for sf2.3

--- Tests/app/sf23.sh
+++ Tests/app/sf23.sh
@@ -2,19 +2,28 @@
 
 VERSION='~2.3.0'
 DIR=$(pwd)
+if [[ "$VERSION" != "$1" ]]; then
+    exit 0;
+fi
+
+move() {
+    rm "$2"
+    if [ -f "$2" ]; then
+        echo "Native $2 file is not deleted"
+        exit 1
+    fi
+    mv "$1" "$2"
+    if [ ! -f "$2" ]; then
+        echo "Mocked file $1 is not moved!"
+        exit 1
+    fi
+    echo "$1 enabled!"
+}
+
 NATIVE="$DIR/Tests/TestBundles/DefaultTestBundle/Entity/CustomizationEntity.php"
 SF23="$DIR/Tests/TestBundles/DefaultTestBundle/Entity/CustomizationEntity_sf_2_3.php"
+move "$SF23" "$NATIVE"
 
-if [[ "$VERSION" == "$1" ]]; then
-    rm "$NATIVE"
-    if [ -f "$NATIVE" ]; then
-        echo "Native file is not deleted!"
-        exit 1
-    fi
-    mv "$SF23" "$NATIVE"
-    if [ ! -f "$NATIVE" ]; then
-        echo "Mocked file is not moved!"
-        exit 1
-    fi
-    echo 'Moved!'
-fi
+NATIVE="$DIR/Tests/TestBundles/DefaultTestBundle/Entity/BasicConstraintsEntity.php"
+SF23="$DIR/Tests/TestBundles/DefaultTestBundle/Entity/BasicConstraintsEntity_sf_2_3.php"
+move "$SF23" "$NATIVE"
