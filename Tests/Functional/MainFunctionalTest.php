798e0140781adf6d97bd94b3cff11d8b9d128c4f
Ton Sharp <github@66ton99.org.ua>
2016-05-26 15:57:06 +0300

    Fix Collection test (#114)

--- Tests/Functional/MainFunctionalTest.php
+++ Tests/Functional/MainFunctionalTest.php
@@ -321,12 +321,12 @@ class MainFunctionalTest extends BaseMinkTestCase
         $addComment->click();
         $submit->click();
 
-        $this->assertEquals($getExpected(4, 3, 0), array_count_values($getErrors()));
+        $this->assertEquals($getExpected(1, 1, 0), array_count_values($getErrors()));
 
         $this->find('#del_form_task_tags_2')->click();
         $this->find('#del_form_task_comments_1')->click();
         $submit->click();
-        $this->assertEquals($getExpected(3, 2, 0), array_count_values($getErrors()));
+        $this->assertEquals($getExpected(1, 1, 0), array_count_values($getErrors()));
 
         $page->findField('form_task_tags_0_title')->setValue('asdf');
         $page->findField('form_task_tags_1_title')->setValue('asdf');
