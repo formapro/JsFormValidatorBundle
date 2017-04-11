8ae797e3ac953f8578270f02539194a38458e380
Honza Gula <honza.gula@gmail.com>
2017-04-11 10:10:51 +0200

    Removed TLD validation by list

--- Resources/public/js/constraints/Email.js
+++ Resources/public/js/constraints/Email.js
@@ -8,7 +8,7 @@ function SymfonyComponentValidatorConstraintsEmail() {
     this.message = '';
 
     this.validate = function (value) {
-        var regexp = /^[-a-z0-9~!$%^&*_=+}{'?]+(\.[-a-z0-9~!$%^&*_=+}{'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+        var regexp = /^[-a-z0-9~!$%^&*_=+}{'?]+(\.[-a-z0-9~!$%^&*_=+}{'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.([a-z]+)|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
         var errors = [];
         var f = FpJsFormValidator;
 
