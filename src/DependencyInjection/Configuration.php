e6c0e131b45f7f687941773cd95fcf32ba0f5d12
Tomas Pesek <tomas.pesek@shopsys.com>
2020-02-11 08:17:46 +0100

    updated configurator to compatible with symfony 4*

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ namespace Fp\JsFormValidatorBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * This is the class that validates and merges configuration from the app/config files
@@ -16,13 +17,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        /*
-         * symfony 4.3 version
-         * $treeBuilder = new TreeBuilder('fp_js_form_validator');
-         * $rootNode = $treeBuilder->getRootNode();
-         */
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('fp_js_form_validator');
+        // A tree builder without a root node is deprecated since Symfony 4.2 and will not be supported anymore in 5.0.
+        if (Kernel::VERSION_ID < 40212) {
+            $treeBuilder = new TreeBuilder();
+            $rootNode = $treeBuilder->root('fp_js_form_validator');
+        } else {
+            $treeBuilder = new TreeBuilder('fp_js_form_validator');
+            $rootNode = $treeBuilder->getRootNode();
+        }
 
         /** @noinspection PhpUndefinedMethodInspection */
         $rootNode
