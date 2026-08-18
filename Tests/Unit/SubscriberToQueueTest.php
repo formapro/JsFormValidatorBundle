2de2e65913f49c1bead564df5e2a501dd61dbb58
Damien ALEXANDRE <dalexandre@jolicode.com>
2026-08-18 17:52:21 +0200

    chore(tests): refactor the factory thing / use a Trait

--- Tests/Unit/SubscriberToQueueTest.php
+++ Tests/Unit/SubscriberToQueueTest.php
@@ -3,22 +3,18 @@
 namespace Fp\JsFormValidatorBundle\Tests\Unit;
 
 use Fp\JsFormValidatorBundle\Factory\JsFormValidatorFactory;
-use Fp\JsFormValidatorBundle\Form\Extension\FormExtension;
 use Fp\JsFormValidatorBundle\Form\Subscriber\SubscriberToQueue;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
 use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\Forms;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Validator\Validation;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SubscriberToQueueTest extends TestCase
 {
+    use FactoryTestTrait;
     public function testAddToQueueWhenGlobalDisabledButLocalExplicitlyEnabled()
     {
-        $factory = $this->createFactory(array('js_validation' => false));
+        $factory = $this->createFactory(null, null, array('js_validation' => false));
         $formFactory = $this->createFormFactory($factory);
         $subscriber = new SubscriberToQueue($factory);
 
@@ -34,7 +30,7 @@ class SubscriberToQueueTest extends TestCase
 
     public function testDoesNotAddToQueueWhenGlobalDisabledAndLocalNotSet()
     {
-        $factory = $this->createFactory(array('js_validation' => false));
+        $factory = $this->createFactory(null, null, array('js_validation' => false));
         $formFactory = $this->createFormFactory($factory);
         $subscriber = new SubscriberToQueue($factory);
 
@@ -51,7 +47,7 @@ class SubscriberToQueueTest extends TestCase
 
     public function testAddToQueueWhenGlobalEnabledAndLocalNotSet()
     {
-        $factory = $this->createFactory(array('js_validation' => true));
+        $factory = $this->createFactory(null, null, array('js_validation' => true));
         $formFactory = $this->createFormFactory($factory);
         $subscriber = new SubscriberToQueue($factory);
 
@@ -68,7 +64,7 @@ class SubscriberToQueueTest extends TestCase
 
     public function testDoesNotAddToQueueWhenLocalExplicitlyDisabled()
     {
-        $factory = $this->createFactory(array('js_validation' => true));
+        $factory = $this->createFactory(null, null, array('js_validation' => true));
         $formFactory = $this->createFormFactory($factory);
         $subscriber = new SubscriberToQueue($factory);
 
@@ -80,25 +76,5 @@ class SubscriberToQueueTest extends TestCase
         $subscriber->onFormSetData(new FormEvent($form, null));
 
         $this->assertFalse($factory->inQueue($form));
-    }
-
-    private function createFactory(array $config = array())
-    {
-        return new JsFormValidatorFactory(
-            Validation::createValidator(),
-            $this->createStub(TranslatorInterface::class),
-            $this->createStub(UrlGeneratorInterface::class),
-            $config,
-            'validators'
-        );
-    }
-
-    private function createFormFactory(JsFormValidatorFactory $factory)
-    {
-        return Forms::createFormFactoryBuilder()
-            ->addExtension(new ValidatorExtension(Validation::createValidator()))
-            ->addTypeExtension(new FormExtension($factory))
-            ->getFormFactory()
-        ;
     }
 }
